dc1a42a24f0bcdcd2b9369aa27cdfbcf587d621e
Diksha <diksha.magicalsolutions@gmail.com>
2023-03-24 10:30:47 +0530

    Generic

---
+++ TextFileReading.java
@@ -0,0 +1,28 @@
+
+ 
+import java.io.FileReader;
+import java.io.IOException;
+ 
+/**
+ * This program demonstrates how to read characters from a text file.
+ * @author www.codejava.net
+ *
+ */
+public class TextFileReading {
+ 
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("MyFile.txt");
+            int character;
+ 
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
+            }
+            reader.close();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+}
